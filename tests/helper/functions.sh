10256a66bebef161efa2cc6896b185f2d81c75f9
cougar <rakesh.iitm@gmail.com>
2017-01-20 16:44:56 +0530

    added provision for global_speed and with locking. Not tested yet

--- tests/helper/functions.sh
+++ tests/helper/functions.sh
@@ -169,3 +169,46 @@ function execute_cmd() {
     $1
   fi
 }
+
+function lock {
+    if [ "$#" -ne 1 ]; then
+        echo 'usage: lock [LOCKFILENAME]' 1>&2
+        return 1
+    fi
+
+    LOCKFILE="$1"
+
+    # make a file with our PID
+    # easy way to show who's waiting for a lock
+    if ! touch "$LOCKFILE.$$" ; then
+        echo "failed to create PID lockfile: $1" 1>&2
+        return 1
+    fi
+
+    # try to symlink it
+    while ! ln "$LOCKFILE.$$" "$LOCKFILE" 2>/dev/null
+    do
+        echo "failed to symlink"
+        # if the symlink failed, wait for the current lock holder to exit
+        sleep 1
+    done
+
+    # symlink was created successfully, lock acquired
+
+    # if the locking process exits with unlocking, delete our lock
+    trap 'rm -f "$LOCKFILE" "$LOCKFILE.$$"' EXIT
+
+    return 0
+}
+
+function unlock {
+    if [ "$#" -ne 1 ]; then
+        echo 'usage: unlock [LOCKFILENAME]' 1>&2
+        return 2
+    fi
+
+    rm -f "$LOCKFILE" # the symlink that locks are competing for
+    rm -f "$LOCKFILE.$$" 
+
+    return 0
+}
